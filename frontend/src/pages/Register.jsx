aaf3b991b01e7902ebab5c8c8670ebaa7dd7ef8f
wycliffe <ericoochieng456@gmail.com>
2024-06-10 07:54:34 +0300

    API

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -21,7 +21,7 @@ function RegisterPage() {
       password: password,
     };
     try {
-      const apiUrl = "http://127.0.0.1:8000/auth/register";
+      const apiUrl = "http://64.227.77.230:8000/auth/register";
       const response = await axios.post(apiUrl, formContent, {
         headers: {
           "Content-Type": "application/json",
